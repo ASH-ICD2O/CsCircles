07657073e2a5a7d9a822291515168e026afb9c31
Jamieson <158116310+ash-comsci@users.noreply.github.com>
2025-01-10 13:42:15 +0000

    test

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 name = input("Welcome to the testing platform, what is your name:  ")
 
-print(f"Welcome, {name], I hope this helps you and you do well in ICD2O. Mr J has really enjoyed your presence in class.")
+print(f"Welcome, {name}, I hope this helps you and you do well in ICD2O. Mr J has really enjoyed your presence in class.")
